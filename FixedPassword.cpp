a891408e409a46182689e3626028b2c94e399a12
The-CapedCrusader <6209tanvir@gmail.com>
2025-05-12 04:35:57 +0600

    Codeforces Factorial

--- FixedPassword.cpp
+++ FixedPassword.cpp
@@ -1,23 +1,29 @@
-https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/D
+//https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/G
 //https://asksenior.in/solve
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    int num;
+    int T;
+    cin >> T;
+    for (int i=0; i<T;i++){
+        long long n;
+        cin >> n; 
+        int fct = 1;
+        for (int j = 1; j<=n;j++){
+            fct=fct*j ;
 
-    while (1)
-    {
-        cin >> num;
-        if (num==1999){
-            cout<<"Correct"<< endl;
-            break;
         }
-        else{
-            cout << "Wrong" << endl; 
-        }
+        cout << fct << endl; 
+
+
+
+
     }
+    
+
+    
      
     return 0; 
 
