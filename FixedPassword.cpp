3ea8eee5b236442447d640b9fcc1c9f5e94b4a6f
The-CapedCrusader <6209tanvir@gmail.com>
2025-05-12 04:19:01 +0600

    Codeforces Fixed Password

---
+++ FixedPassword.cpp
@@ -0,0 +1,26 @@
+https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/D
+//https://asksenior.in/solve
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num;
+
+    while (1)
+    {
+        cin >> num;
+        if (num==1999){
+            cout<<"Correct"<< endl;
+            break;
+        }
+        else{
+            cout << "Wrong" << endl; 
+        }
+    }
+     
+    return 0; 
+
+
+}
+
